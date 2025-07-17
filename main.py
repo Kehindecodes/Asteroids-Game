4c74174a896df43a679fa39934d734dd4e969e7f
Kehinde Oki <olamiposi.oki@gmail.com>
2025-07-17 15:01:38 +0100

    Add player rotation and update logic to handle input

--- main.py
+++ main.py
@@ -8,20 +8,16 @@ from player import Player
 def main ():
   pygame.init()
   time = pygame.time.Clock()
+  player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
   dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  print(screen)
-
   while True :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return
-    pygame.Surface.fill(screen,(0,0,0))
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    player.update(dt)
+    pygame.Surface.fill(screen,( 0,0,0))
     player.draw(screen)
-
-
-
     pygame.display.flip()
     time_passed = time.tick((60))
     dt = time_passed/1000
